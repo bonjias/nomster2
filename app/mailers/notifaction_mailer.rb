f78d4ff262cef9e1091fabd3dba666c0f582bd71
Jonathan Bias <bonjias@gmail.com>
2015-09-02 19:02:03 +0000

    lesson 42 link not working

--- app/mailers/notifaction_mailer.rb
+++ app/mailers/notifaction_mailer.rb
@@ -1,11 +1,12 @@
 class NotifactionMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-def comment_added
-	mail(to: "bonjias@gmail.com",
-		subject: "A comment has been added")
-
-end 
+def comment_added(comment)
+  @place = comment.place
+  @place_owner = @place.user
+  mail(to: @place_owner.email,
+       subject: "A comment has been added to #{@place.name}")
+end
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
- config.action_mailer.default_url_options = {:host => ' https://nomster-bon-jias.herokuapp.com'}
+ config.action_mailer.default_url_options = {:host => ' https://nomster2-bon-jias.herokuapp.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
