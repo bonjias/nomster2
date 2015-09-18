ae39057be0bb66a7151c836858dd19d02b818a1e
Jonathan Bias <bonjias@gmail.com>
2015-09-18 16:32:03 +0000

    close

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -9,7 +9,7 @@ class CommentTest < ActiveSupport::TestCase
   	user = FactoryGirl.create(:user)
   	place = FactoryGirl.create(:place)
 	comment = FactoryGirl.create(:comment)
-	expect = 'one star'
+	expected = 'one star'
 	actual = comment.humanized_rating
 	assert_equal expected,actual
 	end 
